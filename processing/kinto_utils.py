74e1f44e70d9787861a4132da63e6f8e0b12ffad
ubergesundheit <ubergesundheit@googlemail.com>
2019-03-04 19:20:03 +0100

    change check against empty rows in importer

--- processing/kinto_utils.py
+++ processing/kinto_utils.py
@@ -6,13 +6,20 @@ client = Client(server_url=kinto_url,
                 auth=(kinto_admin, kinto_password),
                 retry=10)
 
+required_colums = [
+    'place', 'place_near', 'day_of_week', 'date', 'time_of_day'
+]
+
 
 def create_accident_raw(accident):
     # check for empty row, checking the first 5 columns for
     # emptyness should be enough
-    if all([accident[k] for k in ['place', 'place_near',
-                                  'day_of_week', 'date',
-                                  'time_of_day']]):
+    required_colums_values = [
+        accident[k] for k in required_colums
+        if accident[k] is not None and accident[k] != 'None'
+    ]
+
+    if len(required_colums) == len(required_colums_values):
         client.create_record(data=accident,
                              collection='accidents_raw',
                              bucket='accidents',
