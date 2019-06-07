69560da7a9fb6159c1e70c9be6234bd1c1cfa486
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-07 10:50:32 +0200

    make random-accident query way faster

--- random-accident/webhook/random-accident.sh
+++ random-accident/webhook/random-accident.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-
-COUNT=$(psql -qtAX postgresql://accidents:PASSWORD@postgres/accidents -c "SELECT count(id) FROM objects WHERE resource_name = 'record' AND parent_id = '/buckets/accidents/collections/geometries';")
+# TODO move this into container initialization
+COUNT=$(psql -qtAX postgresql://accidents:PASSWORD@postgres/accidents -c "
+  SELECT count(id) FROM objects
+  WHERE resource_name = 'record'
+    AND parent_id = '/buckets/accidents/collections/geometries';")
 
 psql -qtAX postgresql://accidents:PASSWORD@postgres/accidents -c "
   WITH
-    geometries AS (SELECT * FROM objects WHERE resource_name = 'record' AND parent_id = '/buckets/accidents/collections/geometries'),
-    accidents AS (SELECT * FROM objects WHERE resource_name = 'record' AND parent_id = '/buckets/accidents/collections/accidents_raw'),
-    result AS (SELECT
-      geometries.id AS geometry_id,
-      geometries.data->>'accident_id' AS accident_id,
-      geometries.data->'lat' AS lat,
-      geometries.data->'lon' AS lon,
-      accidents.data->'place' AS place,
-      accidents.data->'place_near' AS place_near,
-      (date(accidents.data->>'date') + TO_TIMESTAMP(accidents.data->>'time_of_day', 'HH24:MI:SS')::time) AS timestamp
-    FROM geometries JOIN accidents ON geometries.data->>'accident_id' = accidents.id OFFSET floor(random()*${COUNT}) LIMIT 1)
+    geometries AS (
+      SELECT
+        id as geometry_id,
+        data->>'accident_id' AS accident_id,
+        data->'lat' AS lat,
+        data->'lon' AS lon
+      FROM objects
+      WHERE resource_name = 'record'
+        AND parent_id = '/buckets/accidents/collections/geometries'
+      OFFSET floor(random()*${COUNT}) LIMIT 1),
+    result AS (
+      SELECT
+        g.geometry_id,
+        g.accident_id,
+        g.lat,
+        g.lon,
+        a.data->'place' AS place,
+        a.data->'place_near' AS place_near
+      FROM objects AS a, geometries AS g
+      WHERE a.resource_name = 'record'
+        AND a.parent_id = '/buckets/accidents/collections/accidents_raw'
+        AND a.id = g.accident_id)
   SELECT row_to_json(result) FROM result;
 "
