317da0a2bb68b082b37c1dbfeb5509967fcae693
ubergesundheit <ubergesundheit@googlemail.com>
2019-08-27 19:14:32 +0200

    don't create admin account if it exists in init script

--- kinto/init.sh
+++ kinto/init.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
 
-echo "creating admin account"
+AUTHORIZATION=$(echo -n "admin:${KINTO_ADMIN_PASSWORD}" | base64)
+
+curl \
+  --silent -o /dev/null \
+  --fail \
+  -H "Authorization: Basic ${AUTHORIZATION}" \
+  "${KINTO_URL}/accounts/admin" || (echo "creating admin account";
 curl -XPUT \
-   --fail \
+  --fail \
+  --silent \
   -H 'content-type: application/json' \
   -d "{\"data\": {\"password\": \"${KINTO_ADMIN_PASSWORD}\"}}" \
-  "${KINTO_URL}/accounts/admin"
+  "${KINTO_URL}/accounts/admin"; echo)
 
-echo
 echo "importing schema"
 kinto-wizard load \
   --server "${KINTO_URL}" \
